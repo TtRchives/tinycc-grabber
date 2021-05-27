95edbc3c0b30d4965fccc7f64520ffdf44b0c04d
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-05-27 14:56:57 -0400

    remove debug stuff

--- URLs.py
+++ URLs.py
@@ -6,7 +6,7 @@ try:
         urls = json.load(file)
 except Exception:
     urls = {}
-for i in range(0,random.randint(0,10)):
+for i in range(0,random.randint(1,10)):
     url += chr(random.choice((random.randint(65,90), random.randint(97,122), random.randint(48,57)))) #first one: uppercase; second one: lowercase; third one: numebes
 print("Pinging URL %s"%url)
 j=subprocess.Popen([("""curl -w "%%{url_effective}\n" -I -L -s -S tiny.cc/%s -o /dev/null"""%url)], shell=True, executable="/bin/bash", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -16,8 +16,6 @@ if j.returncode != 0:
     sys.exit(1)
 print(stuff)
 urls[url] = stuff[0].decode("utf-8").replace("\n","")
-print(urls[url])
-print(f"tiny.cc/{url}")
 if urls[url] == f"tiny.cc/{url}" or urls[url] == f"https://tiny.cc/{url}" or urls[url] == f"http://tiny.cc/{url}":
     print("Nonexistent URL")
     sys.exit(2)
