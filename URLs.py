TtRchives/tinycc-grabber

commit c275e3e09590716aef5b88a13eb47cdd2b9297d5

fix edge case

URLs.py
@@ -9,8 +9,9 @@ class URL:
         self.GetConfig()
     def GetURL(self, urlRange=(1,10)):
         self.url = ""
-        for i in range(0,random.randint(*urlRange)):
-            self.url += chr(random.choice((random.randint(65,90), random.randint(97,122), random.randint(48,57)))) #first one is uppercase; 2nd is lowercase; 3rd: numbers
+        while self.url in list(self.urls) or self.url == "":
+            for i in range(0,random.randint(*urlRange)):
+                self.url += chr(random.choice((random.randint(65,90), random.randint(97,122), random.randint(48,57)))) #first one is uppercase; 2nd is lowercase; 3rd: numbers
     def GetConfig(self):
         try:
             with open("file.json") as file:

.gitignore
@@ -1,1 +1,2 @@
 __pycache__/
+file.json

commit dbe0c3dd86315ab51beba0a2e025d523e778f8f4

fix fix edge case
fixes bug caused in c275e3e

URLs.py
@@ -10,6 +10,7 @@ class URL:
     def GetURL(self, urlRange=(1,10)):
         self.url = ""
         while self.url in list(self.urls) or self.url == "":
+            self.url = ""
             for i in range(0,random.randint(*urlRange)):
                 self.url += chr(random.choice((random.randint(65,90), random.randint(97,122), random.randint(48,57)))) #first one is uppercase; 2nd is lowercase; 3rd: numbers
     def GetConfig(self):